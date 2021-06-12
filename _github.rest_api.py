e5838cd16148aa27bcff88b75189d44ce2a56216
Alper Canimoglu <79938189+CnmAlper@users.noreply.github.com>
2021-06-12 21:40:17 +0300

    Update _github.rest_api.py

--- _github.rest_api.py
+++ _github.rest_api.py
@@ -4,8 +4,8 @@ import json
 
 class Github:
     def __init__(self):
-        self.api_url = 'https://api.github.com'
-        self.token = 'your_token'
+        self.api_url = 'your_api_url'  # please enter your api url.
+        self.token = 'your_token'      # please enter your token. 
 
     def getUser(self, username):
         response = requests.get(self.api_url+'/users/'+username)
